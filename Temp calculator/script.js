576ec9aa16f44f4142a47ab87070cf2a58088cb9
Saivivek-velpula <vvelpulavenkatasai@email.com>
2026-08-12 09:34:19 +0530

    temperature calculator

---
+++ Temp calculator/script.js
@@ -0,0 +1,22 @@
+const celsiusEl = document.getElementById("celsius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
+
+function computeTemp(event) {
+    const currentValue = +event.target.value;
+
+    switch(event.target.id){
+        case "celsius":
+            fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
+            kelvinEl.value = (currentValue + 273.15).toFixed(2);
+            break;
+        case "fahrenheit":
+            celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
+            kelvinEl.value = ((currentValue - 32) / 1.8 + 273.15).toFixed(2);
+            break;
+        case "kelvin":
+            celsiusEl.value = (currentValue - 273.15).toFixed(2);
+            fahrenheitEl.value = ((currentValue - 273.15) * 1.8 + 32).toFixed(2);
+            break;
+    }
+}
